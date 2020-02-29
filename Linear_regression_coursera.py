39faee960cd09760a032556acb44cafcb746d80f
akritigup97 <61154588+akritigup97@users.noreply.github.com>
2020-02-29 13:32:19 +0530

    Update Linear_regression_coursera.py

--- Linear_regression_coursera.py
+++ Linear_regression_coursera.py
@@ -1,59 +1,54 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2511
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
-\f0\fs24 \cf0 import matplotlib.pyplot as plt \
-import pandas as pd\
-import pylab as pl\
-import numpy as np\
-%matplotlib inline\
-!wget -O FuelConsumption.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv\
-df = pd.read_csv("FuelConsumption.csv")\
-\
+
+import matplotlib.pyplot as plt 
+import pandas as pd
+import pylab as pl
+import numpy as np
+%matplotlib inline
+!wget -O FuelConsumption.csv https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/ML0101ENv3/labs/FuelConsumptionCo2.csv
+df = pd.read_csv("FuelConsumption.csv")
+
 # take a look at the dataset\
-df.head()\
-df.describe()\
-cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]\
-cdf.head(9)\
-viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]\
-viz.hist()\
-plt.show()\
-plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')\
-plt.xlabel("FUELCONSUMPTION_COMB")\
-plt.ylabel("Emission")\
-plt.show()\
-plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='red')\
-plt.xlabel("Engine size")\
-plt.ylabel("Emission")\
-plt.show()\
-msk = np.random.rand(len(df)) < 0.8\
-train = cdf[msk]\
-test = cdf[~msk]\
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='green')\
-plt.xlabel("Engine size")\
-plt.ylabel("Emission")\
-plt.show()\
-from sklearn import linear_model\
-regr = linear_model.LinearRegression()\
-train_x = np.asanyarray(train[['ENGINESIZE']])\
-train_y = np.asanyarray(train[['CO2EMISSIONS']])\
-regr.fit (train_x, train_y)\
-# The coefficients\
-print ('Coefficients: ', regr.coef_)\
-print ('Intercept: ',regr.intercept_)\
-plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')\
-plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')\
-plt.xlabel("Engine size")\
-plt.ylabel("Emission")\
-from sklearn.metrics import r2_score\
-\
-test_x = np.asanyarray(test[['ENGINESIZE']])\
-test_y = np.asanyarray(test[['CO2EMISSIONS']])\
-test_y_hat = regr.predict(test_x)\
-\
-print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))\
-print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))\
-print("R2-score: %.2f" % r2_score(test_y_hat , test_y) )}
+df.head()
+df.describe()
+cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
+cdf.head(9)
+viz = cdf[['CYLINDERS','ENGINESIZE','CO2EMISSIONS','FUELCONSUMPTION_COMB']]
+viz.hist()
+plt.show()
+plt.scatter(cdf.FUELCONSUMPTION_COMB, cdf.CO2EMISSIONS,  color='blue')
+plt.xlabel("FUELCONSUMPTION_COMB")
+plt.ylabel("Emission")
+plt.show()
+plt.scatter(cdf.ENGINESIZE, cdf.CO2EMISSIONS,  color='red')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+msk = np.random.rand(len(df)) < 0.8
+train = cdf[msk]
+test = cdf[~msk]
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='green')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+plt.show()
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit (train_x, train_y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+print ('Intercept: ',regr.intercept_)
+plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
+plt.plot(train_x, regr.coef_[0][0]*train_x + regr.intercept_[0], '-r')
+plt.xlabel("Engine size")
+plt.ylabel("Emission")
+from sklearn.metrics import r2_score
+
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_hat = regr.predict(test_x)
+
+print("Mean absolute error: %.2f" % np.mean(np.absolute(test_y_hat - test_y)))
+print("Residual sum of squares (MSE): %.2f" % np.mean((test_y_hat - test_y) ** 2))
+print("R2-score: %.2f" % r2_score(test_y_hat , test_y) )
